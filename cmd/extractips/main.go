19ddd7c63f57cc6bdc10f57dab4b65e9c0783858
peron <peron_ap@icloud.com>
2024-05-26 23:42:14 +0100

    feat: extract ips based on country

---
+++ cmd/extractips/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/oschwald/geoip2-golang"
+	"github.com/oschwald/maxminddb-golang"
+)
+
+func main() {
+	dbpath := flag.String("db", "", "The filename of the MaxMind GeoLite2 database in mmdb format")
+	savepath := flag.String("o", "ip.txt", "The filename to save the extracted IPs")
+	countryCode := flag.String("c", "", "The country code to extract")
+	ipv4Only := flag.Bool("4", false, "Only extract IPv4 addresses")
+
+	flag.Parse()
+
+	if *dbpath == "" || *countryCode == "" {
+		flag.PrintDefaults()
+		return
+	}
+	*countryCode = strings.ToUpper(*countryCode)
+
+	// Open the MaxMind database
+	reader, err := maxminddb.Open(*dbpath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer reader.Close()
+
+	// Open the save file
+	file, err := os.OpenFile(*savepath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer file.Close()
+
+	// Extract the IPs
+	ipCount := 0
+	networks := reader.Networks()
+
+	record := geoip2.Country{}
+	for networks.Next() {
+		subnet, err := networks.Network(&record)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		if record.Country.IsoCode == *countryCode {
+			if *ipv4Only && subnet.IP.To4() == nil {
+				continue
+			}
+			ones, bits := subnet.Mask.Size()
+			ipCount += 1 << uint(bits-ones)
+			file.WriteString(subnet.String() + "\n")
+		}
+	}
+
+	log.Printf("Extracted %d IPs\n", ipCount)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/PeronGH/findcf
 
 go 1.22.2
+
+require (
+	github.com/oschwald/geoip2-golang v1.9.0
+	github.com/oschwald/maxminddb-golang v1.12.0
+)
+
+require golang.org/x/sys v0.10.0 // indirect
